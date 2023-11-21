16656f8a1862a560284a33250d23eb73b28af2a6
Michael Avagianos <mixalisava@gmail.com>
2023-11-21 18:07:41 +0200

    feat: change CMakeList and main.c

---
+++ CMakeFiles/di_os_2023_24.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/di_os_2023_24.dir/erg2/main.c.o"
+  "CMakeFiles/di_os_2023_24.dir/erg2/main.c.o.d"
+  "erg2/mycp"
+  "erg2/mycp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/di_os_2023_24.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- erg2/main.c
+++ erg2/main.c
@@ -9,17 +9,17 @@ int main (int argc, char *argv[]) {
 
     if (argc != 3) {
         printf("Usage: %s <source> <destination>\n", argv[0]);
-        return 1;
+        exit(1);
     }
 
     if ((fd1 = open(argv[1], O_RDONLY)) == -1) {
         perror(argv[1]);
-        return 2;
+        exit(2);
     }
 
     if ((fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1) {
         perror(argv[2]);
-        return 3;
+        exit(3);
     }
 
     while (read(fd1, &buf, 1) > 0) {
