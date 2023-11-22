33c67e49391ab7d79c33d771ba9d6668b0d9b4f3
Lost <pandagaminggr@gmail.com>
2023-11-22 17:29:25 +0200

    feat: add more buffer sizes; refactor

--- erg2/main.c
+++ erg2/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
 #include "mycp.h"
 
 int main(int argc, char *argv[]) {
--- script.sh
+++ script.sh
@@ -15,8 +15,8 @@ touch output
 cd out/
 # No buffer (4096 bytes)
 time ./mycp ../input ../output >> report.out
-# Run through 16-16384 buffer sizes
-for i in 16 32 64 128 512 1024 2048 8192 16384
+# Run through 8-16384 buffer sizes
+for i in 8 16 32 64 128 512 1024 2048 8192 16384
 do
   dd if=/dev/urandom of=../input bs=16M count=1 iflag=fullblock
   time ./mycp -b$i ../input ../output >> report.out
