82f40e84503af1d4e77d9a11241f0672331d3093
Lost <pandagaminggr@gmail.com>
2024-01-07 18:55:43 +0200

    feat: implement erg4 functionality

--- erg4/main.c
+++ erg4/main.c
@@ -1,3 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 int main() {
-  return 0;
+    while(1) {
+      for (int i = 0; i < 3; i++) {
+          pid_t pid = fork();
+          if (pid == 0) {
+              // Child process 1
+              if (i == 0) {
+                  printf("ThisIsASeriesOfSymbols!%d\n", getpid());
+                  exit(0);
+              }
+              // Child process 2
+              if (i == 1) {
+                  printf("ThisIsASeriesOfSymbols!!%d\n", getpid());
+                  exit(0);
+              }
+              // Child process 3
+              if (i == 2) {
+                  printf("ThisIsASeriesOfSymbols%d\n", getpid());
+                  exit(0);
+              }
+              exit(0);
+          } else if (pid > 0) {
+              // Parent process
+              wait(NULL); // Wait for child process to finish
+          } else {
+              // Fork failed
+              printf("Fork failed (500)\n");
+              exit(500);
+          }
+      }
+    }
+    exit(0);
 }
