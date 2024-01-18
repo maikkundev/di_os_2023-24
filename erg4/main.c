d12cc202de87f7887a3e9f0935a537cada98302d
Lost <pandagaminggr@gmail.com>
2024-01-18 15:58:33 +0200

    feat: add semaphores

--- erg4/main.c
+++ erg4/main.c
@@ -2,42 +2,57 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <fcntl.h>
 
 int main() {
-  FILE *file = fopen("output.txt", "a");
+    FILE *file = fopen("output.txt", "a");
 
-  while (1) {
-    for (int i = 0; i < 3; i++) {
-      pid_t pid = fork();
-      if (pid == 0) {
-        // Child process 1
-        if (i == 0) {
-          printf("ThisIsASeriesOfSymbols!%d\n", getpid());
-          fprintf(file, "ThisIsASeriesOfSymbols!%d\n", getpid());
-          exit(0);
+    // Create semaphores
+    sem_t *sem1 = sem_open("/sem1", O_CREAT, 0644, 1);
+    sem_t *sem2 = sem_open("/sem2", O_CREAT, 0644, 0);
+    sem_t *sem3 = sem_open("/sem3", O_CREAT, 0644, 0);
+
+    while (1) {
+        for (int i = 0; i < 3; i++) {
+            pid_t pid = fork();
+            if (pid == 0) {
+                // Child process
+                if (i == 0) {
+                    sem_wait(sem1);
+                    printf("ThisIsASeriesOfSymbols!%d\n", getpid());
+                    fprintf(file, "ThisIsASeriesOfSymbols!%d\n", getpid());
+                    sem_post(sem2);
+                } else if (i == 1) {
+                    sem_wait(sem2);
+                    printf("ThisIsASeriesOfSymbols!!%d\n", getpid());
+                    fprintf(file, "ThisIsASeriesOfSymbols!!%d\n", getpid());
+                    sem_post(sem3);
+                } else if (i == 2) {
+                    sem_wait(sem3);
+                    printf("ThisIsASeriesOfSymbols%d\n", getpid());
+                    fprintf(file, "ThisIsASeriesOfSymbols%d\n", getpid());
+                    sem_post(sem1);
+                }
+                exit(0);
+            } else if (pid > 0) {
+                // Parent process
+                wait(NULL); // Wait for child process to finish
+            } else {
+                // Fork failed
+                printf("Fork failed (500)\n");
+                exit(500);
+            }
         }
-        // Child process 2
-        if (i == 1) {
-          printf("ThisIsASeriesOfSymbols!!%d\n", getpid());
-          fprintf(file, "ThisIsASeriesOfSymbols!!%d\n", getpid());
-          exit(0);
-        }
-        // Child process 3
-        if (i == 2) {
-          printf("ThisIsASeriesOfSymbols%d\n", getpid());
-          fprintf(file, "ThisIsASeriesOfSymbols%d\n", getpid());
-          exit(0);
-        }
-        exit(0);
-      } else if (pid > 0) {
-        // Parent process
-        wait(NULL); // Wait for child process to finish
-      } else {
-        // Fork failed
-        printf("Fork failed (500)\n");
-        exit(500);
-      }
     }
-  }
-  exit(0);
+
+    // Close and unlink semaphores
+    sem_close(sem1);
+    sem_close(sem2);
+    sem_close(sem3);
+    sem_unlink("/sem1");
+    sem_unlink("/sem2");
+    sem_unlink("/sem3");
+
+    exit(0);
 }
